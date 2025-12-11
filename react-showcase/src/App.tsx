2449eb54b1cc2c5c8d1cde99a226ac8aa25841be
cjescalona1 <cjescalona1@gmail.com>
2025-12-11 19:27:46 -0400

    react-router-dom installation , simple set of routes to show main route and childrens

--- react-showcase/src/App.tsx
+++ react-showcase/src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Link, Outlet } from 'react-router-dom'
+import './index.css' // Ensure styles are applied
 
 function App() {
-  const [count, setCount] = useState(0)
+  const links = [
+    { to: '/home', label: 'Home' },
+    { to: '/list', label: 'List' },
+    //  { to: '/about', label: 'About' },
+  ]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app-container">
+      <header className="app-header">React Showcase</header>
+      <aside className="app-sidebar">
+        <nav>
+          {links.map((link) => (
+            <Link key={link.to} to={link.to}>
+              {link.label}
+            </Link>
+          ))}
+        </nav>
+      </aside>
+      <main className="app-content"><Outlet /></main>
+      <footer className="app-footer">...</footer>
+    </div>
   )
 }
 
--- react-showcase/src/main.tsx
+++ react-showcase/src/main.tsx
@@ -2,9 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/home',
+        element: <h1>Home</h1>,
+      },
+      {
+        path: '/list',
+        element: <h1>List</h1>,
+      },
+    ]
+  },
+  {
+    path: '/about',
+    element: <h1>About</h1>,
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
